9e42b0aa56503ea65e9b2786893db83b46e4623f
James Galantino <james_galantino@aol.com>
2021-03-12 18:24:12 -0500

    Added axios request for randomuser API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import axios from 'axios';
 import React from 'react';
 
-function App() {
-  return <div className='App'></div>;
+class App extends React.Component {
+  state = {
+    employee: null,
+  };
+
+  componentDidMount() {
+    this.getEmployees();
+  }
+
+  getEmployees = async () => {
+    const { data } = await axios.get('https://randomuser.me/api/?results=500', {
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    console.log(data);
+    this.setState({ employee: data });
+  };
+
+  render() {
+    console.log(this.state.employee);
+    return <div className='App'></div>;
+  }
 }
 
 export default App;
