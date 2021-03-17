5a413172a12844c4dda5b2a93813e5dbb2976177
James Galantino <james_galantino@aol.com>
2021-03-16 23:25:26 -0400

    Added a README.md, created index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <nav className='navbar navbar-light bg-light'>
+    <nav className='header navbar navbar-light bg-light'>
       <div className='container-fluid'>
         <span className='navbar-brand mb-0 h1'>Employee Directory</span>
       </div>
